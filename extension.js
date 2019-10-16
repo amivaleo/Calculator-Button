67eafe090403a83776ed115073d18b09e9d10356
loicvh <loicvanhoorebeeck@gmail.com>
2019-10-16 16:16:30 +0200

    keeping enable functi

--- extension.js
+++ extension.js
@@ -55,19 +55,23 @@ function _getWindowActor() {
 function init(extensionMeta) {
 }
 
-function enable () {
-    button = new St.Bin({ style_class: 'panel-button',
-                          reactive: true,
-                          can_focus: true,
-                          x_fill: true,
-                          y_fill: false,
-                          track_hover: true });
-    let icon = new St.Icon({ icon_name: 'accessories-calculator-symbolic',
-                             style_class: 'system-status-icon' });
-    
-    button.set_child(icon);
-    button.connect('button-press-event', toggleCalculator);
-    Main.panel._rightBox.insert_child_at_index(button, 0);
+function enable() {
+	button = new St.Bin({
+		style_class: 'panel-button',
+		reactive: true,
+		can_focus: true,
+		x_fill: true,
+		y_fill: false,
+		track_hover: true});
+
+	let icon = new St.Icon({
+		icon_name: 'accessories-calculator-symbolic',
+		style_class: 'system-status-icon'});
+
+	button.set_child(icon);
+	button.connect('button-press-event', toggleCalculator);
+
+	Main.panel._rightBox.insert_child_at_index(button, 0);
 }
 
 function disable() {
